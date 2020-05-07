484f4f8622908844a69e2a8e69fb5826477b2cda
Stef van Esch <stef@marshmallow.dev>
2020-05-07 16:51:47 +0200

    Added a Language model to the localeRoute now also works

--- src/app/Traits/TranslatableRoute.php
+++ src/app/Traits/TranslatableRoute.php
@@ -3,17 +3,40 @@
 namespace Marshmallow\MultiLanguage\Traits;
 
 use Illuminate\Support\Facades\App;
+use Marshmallow\HelperFunctions\Facades\URL;
+use Marshmallow\MultiLanguage\Models\Language;
 
 trait TranslatableRoute
 {
-	public function route ()
+	public function localeRoute (Language $language = null)
 	{
-		return '/' . $this->getLocaleString() . $this->routePrefix() . $this->getModelUrl();
+		return URL::buildFromArray(
+			$this->getRouteParts($language)
+		);
 	}
 
-	protected function getModelUrl ()
+	protected function getRouteParts (Language $language = null)
 	{
-		return $this->slug;
+		return array_filter([
+			$this->getLocaleString($language),
+			$this->routePrefix(),
+			$this->getModelUrl($language),
+		]);
+	}
+
+	protected function getModelUrlField ()
+	{
+		return $this->getRouteKeyName();
+	}
+
+	protected function getModelUrl (Language $language = null)
+	{
+		$url_column = $this->getModelUrlField();
+		if ($language) {
+			return $this->getTranslation($url_column, $language->code);
+		}
+
+		return $this->{$url_column};
 	}
 
 	protected function routePrefix ()
@@ -21,8 +44,11 @@ trait TranslatableRoute
 		return '';
 	}
 
-	protected function getLocaleString ()
+	protected function getLocaleString (Language $language = null)
 	{
+		if ($language) {
+			return $language->code;
+		}
 		return App::getLocale();
 	}
 }
